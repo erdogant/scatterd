10483666c8f70fa742ca8c51d1d662963d65899f
erdogant <erdogant@gmail.com>
2020-01-19 17:51:49 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # scatterd
-Easy and fast manner of creating scatter plots
+
+[![Python](https://img.shields.io/pypi/pyversions/scatterd)](https://img.shields.io/pypi/pyversions/scatterd)
+[![PyPI Version](https://img.shields.io/pypi/v/scatterd)](https://pypi.org/project/scatterd/)
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](https://github.com/erdogant/scatterd/blob/master/LICENSE)
+
+* Easy and fast manner of creating scatter plots.
+
+## Contents
+- [Installation](#-installation)
+- [Requirements](#-Requirements)
+- [Quick Start](#-quick-start)
+- [Contribute](#-contribute)
+- [Citation](#-citation)
+- [Maintainers](#-maintainers)
+- [License](#-copyright)
+
+## Installation
+* Install scatterd from PyPI (recommended). scatterd is compatible with Python 3.6+ and runs on Linux, MacOS X and Windows. 
+* It is distributed under the MIT license.
+
+## Requirements
+* It is advisable to create a new environment. 
+```python
+conda create -n env_scatterd python=3.6
+conda activate env_scatterd
+pip install numpy pandas tqdm matplotlib
+```
+
+## Quick Start
+```
+pip install scatterd
+```
+
+* Alternatively, install scatterd from the GitHub source:
+```bash
+git clone https://github.com/erdogant/scatterd.git
+cd scatterd
+python setup.py install
+```  
+
+### Import scatterd package
+```python
+import scatterd as scatterd
+```
+
+### Example:
+```python
+# Import some example data
+from sklearn import datasets
+iris = datasets.load_iris()
+X = iris.data[:, :2]  # we only take the first two features.
+y = iris.target
+
+# Make simple scatterplot
+scatterd(X[:,0], X[:,1])
+# Color based on labels
+scatterd(X[:,0], X[:,1], label=y, s=100)
+# Set labels
+scatterd(X[:,0], X[:,1], label=y, s=100, norm=True, cmap='Set2', xlabel='xlabel', ylabel='ylabel', title='Title')
+# Change sizes
+s=np.random.randint(10,200,len(y))
+scatterd(X[:,0], X[:,1], label=y, s=s, cmap='Set2', xlabel='xlabel', ylabel='ylabel', title='Title', fontsize=25, figsize=(15,10))
+# Change figure size
+scatterd(X[:,0], X[:,1], figsize=(25,15))
+
+```
+<p align="center">
+  <img src="https://github.com/erdogant/scatterd/blob/master/docs/figs/fig1.png" width="600" />
+  <img src="https://github.com/erdogant/scatterd/blob/master/docs/figs/fig2.png" width="600" />
+  <img src="https://github.com/erdogant/scatterd/blob/master/docs/figs/fig3.png" width="600" />
+  <img src="https://github.com/erdogant/scatterd/blob/master/docs/figs/fig4.png" width="600" />
+</p>
+
+
+## Citation
+Please cite scatterd in your publications if this is useful for your research. Here is an example BibTeX entry:
+```BibTeX
+@misc{erdogant2019scatterd,
+  title={scatterd},
+  author={Erdogan Taskesen},
+  year={2019},
+  howpublished={\url{https://github.com/erdogant/scatterd}},
+}
+```
+
+## Maintainers
+* Erdogan Taskesen, github: [erdogant](https://github.com/erdogant)
+
+## Contribute
+* Contributions are welcome.
+
+## Licence
+See [LICENSE](LICENSE) for details.
---
+++ scatterd/scatterd.py
@@ -0,0 +1,141 @@
+'''Easy and fast manner of creating scatter plots.
+
+ from scatterd import scatterd
+ 
+
+ Requirements
+ ------------
+   numpy
+   matplotlib
+   colourmap
+
+
+ Name        : scatterd.py
+ Author      : E.Taskesen
+ Contact     : erdogant@gmail.com
+ Date        : Jan. 2020
+ Licence     : MIT
+
+'''
+
+#%% Libraries
+import colourmap as colourmap
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+#%% Main
+def scatterd(Xcoord, Ycoord, s=15, c=[0,0,0], label=None, norm=False, cmap='Set1', xlabel=None, ylabel=None, title=None, fontsize=16, figsize=(15,10)):
+    '''
+    
+
+    Parameters
+    ----------
+    Xcoord : numpy array
+        1D Coordinates.
+    Ycoord : numpy array
+        1D Coordinates.
+    label: list of labels with same size as Xcoord
+        label of the samples.
+    s: Int or list/array of sizes with same size as Xcoord
+        Size of the samples.
+    c: list/array of RGB colors with same size as Xcoord
+        Color of samples in RGB colors.
+    cmap : String, optional
+        'Set1'       (default)     
+        'Set2'       
+        'rainbow'
+        'bwr'        Blue-white-red
+        'binary' or 'binary_r'
+        'seismic'    Blue-white-red 
+        'Blues'      white-to-blue
+        'Reds'       white-to-red
+        'Pastel1'    Discrete colors
+        'Paired'     Discrete colors
+        'Set1'       Discrete colors    
+    xlabel : String, optional
+        Xlabel. The default is None.
+    ylabel : String, optional
+        Ylabel. The default is None.
+    title : String, optional
+        Title of the figure. The default is None.
+    norm : Bool, optional
+        Normalize datapoints. The default is True.
+    figsize : tuple, optional
+        Figure size. The default is (15,10).
+    fontsize : String, optional
+        The fontsize of the y labels that are plotted in the graph. The default is 16.
+
+
+    References
+    -------
+    Colormap: https://matplotlib.org/examples/color/colormaps_reference.html
+
+
+    Returns
+    -------
+    Fig, ax
+
+
+    '''
+    assert len(Xcoord)==len(Ycoord), print('[SCATTERD] Error. Xcoord should have same length as Ycoord.')
+    assert s is not None, print('[SCATTERD] Error. s(ize) should be not None')
+    assert c is not None, print('[SCATTERD] Error. c(olors) should be not None')
+    if not isinstance(s, int):
+        assert len(s)==len(Xcoord), print('[SCATTERD] Error. s should be of same size of Xcoord')
+
+    args=dict()
+    args['norm']=norm
+    args['cmap']=cmap
+    args['xlabel']=xlabel
+    args['ylabel']=ylabel
+    args['title']=title
+    args['fontsize']=fontsize
+    args['figsize']=figsize
+    # Color of the axis and grid of the plot
+    axiscolor='#dddddd'
+
+    # Combine into array
+    X=np.c_[Xcoord,Ycoord]
+
+    # Normalize data
+    if args['norm']:
+        x_min, x_max = np.min(X, 0), np.max(X, 0)
+        X = (X - x_min) / (x_max - x_min)
+    
+    # Create unqiue colors for label
+    if label is not None:
+        c,cdict=colourmap.fromlist(label, cmap=args['cmap'], method='matplotlib')
+
+    # Boot figure
+    fig, ax = plt.subplots(figsize=args['figsize'])
+    # Scatter
+    ax.scatter(X[:,0],X[:,1], facecolor=c, s=s, edgecolor='None')
+
+    # Plot labels
+    if label is not None:
+        for uilabel in cdict.keys():
+            XYmean=np.mean(X[label==uilabel,:], axis=0)
+            plt.text(XYmean[0],XYmean[1], str(uilabel), color=cdict.get(uilabel), fontdict={'weight': 'bold', 'size': args['fontsize']})
+
+    # Labels on axis
+    ax.set_xlabel(args['xlabel'])
+    ax.set_ylabel(args['ylabel'])
+    if args['title'] is not None:
+        ax.set_title(args['title'])
+        
+    # set background to none
+    ax.set_facecolor('none')
+    # Set grid and axis to grey
+    ax.grid(True, color=axiscolor)
+    for child in ax.get_children():
+        if isinstance(child, matplotlib.spines.Spine):
+            child.set_color(axiscolor)
+
+    # Show figure
+    fig.show()
+    fig.canvas.draw()
+    # Return
+    return(fig,ax)
+
+
