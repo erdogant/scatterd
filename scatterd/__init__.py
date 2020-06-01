9edfb5f60f93b09d5912395cb4e3de251289feb5
Erdogan Taskesen <erdogant@gmail.com>
2020-06-01 16:00:20 +0200

    new version number

--- scatterd/__init__.py
+++ scatterd/__init__.py
@@ -2,14 +2,24 @@ from scatterd.scatterd import scatterd
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 # module level doc-string
 __doc__ = """
-scatterd - Easy and fast way of creating scatter plots.
+scatterd
 =====================================================================
 
-**scatterd** 
-See README.md file for more information.
+Description
+-----------
+Scatterd is an easy and fast way of creating scatter plots.
+
+Example
+-------
+>>> from scaterd import scatterd
+>>> scatterd(X,Y)
+
+References
+----------
+https://github.com/erdogant/scatterd
 
 """
