0646046ed4a8514c83fc7e59f79ce2befc853106
erdogant <erdogant@gmail.com>
2022-06-03 10:48:44 +0200

    fix in case sample size is too small to create density colors

--- scatterd/__init__.py
+++ scatterd/__init__.py
@@ -2,7 +2,7 @@ from scatterd.scatterd import scatterd,import_example, set_colors, _preprocessin
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 # module level doc-string
 __doc__ = """
