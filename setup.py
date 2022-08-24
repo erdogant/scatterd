37f33f658c133a360b309fc5f0b44cf1ab24e147
Hirokazu Takaya <7270139+lisphilar@users.noreply.github.com>
2022-08-25 01:26:32 +0900

    fix: cyclic dependencies
    by removing itself from requirements.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     install_requires=['matplotlib','numpy','colourmap','seaborn','scatterd'],
+     install_requires=['matplotlib','numpy','colourmap','seaborn'],
      python_requires='>=3',
      name='scatterd',
      version=new_version,
