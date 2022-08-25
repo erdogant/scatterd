ba6bcb5966da6277fdb959132dcee9a4e1306195
erdogant <erdogant@gmail.com>
2022-08-25 12:55:51 +0200

    1.1.3

--- scatterd/__init__.py
+++ scatterd/__init__.py
@@ -2,7 +2,7 @@ from scatterd.scatterd import scatterd,import_example, set_colors, _preprocessin
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 
 # module level doc-string
 __doc__ = """
