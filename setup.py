0597697cc69e8e509414301752757adb8c05d288
erdogant <erdogant@gmail.com>
2022-11-01 21:54:08 +0100

    1.2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     install_requires=['matplotlib','numpy','colourmap','seaborn'],
+     install_requires=['matplotlib','numpy','colourmap','seaborn','requests'],
      python_requires='>=3',
      name='scatterd',
      version=new_version,
