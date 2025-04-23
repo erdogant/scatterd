db34c78109568b176143064684627c103adb830a
erdogant <erdogant@gmail.com>
2025-04-23 22:23:58 +0200

    add new unit test

---
+++ scatterd/tests/test_scatterd.py
@@ -0,0 +1,21 @@
+import unittest
+from scatterd import scatterd
+import numpy as np
+from sklearn import datasets
+import matplotlib as mpl
+
+class Testscatterd(unittest.TestCase):
+
+    def test_import_example(self):
+        iris = datasets.load_iris()
+        X = iris.data[:, :2]  # we only take the first two features.
+        labels = iris.target
+        custom_cmap = mpl.colors.ListedColormap(['green', 'black', 'blue'])
+        
+        s = (labels+1) * 200
+        random_integers = np.random.randint(0, len(s), size=X.shape[0])
+        alpha = np.random.rand(1, X.shape[0])[0][random_integers]
+        
+        fig, ax = scatterd(X[:,0], X[:,1], labels=labels, marker=labels, gradient='#ffffff', edgecolor='#ffffff', s=s, density=True, alpha=1, cmap=custom_cmap, density_on_top=False, visible=True)
+        assert ax is not None
+        assert fig is not None
